59137aca7ff045ae1d7f9675c7cbba5cffd38919
Kali <kali@kali>
2021-12-14 05:46:53 -0500

    modify aquascript

--- aquascript.sh
+++ aquascript.sh
@@ -1,8 +1,8 @@
-read -p "Enter the subnet or hostlist: " victim
+read -p "Enter hostlist: " victim
 read -p "Enter the IP of the jVision Server: " jvis
-nmap -sn "$victim" -oG - | awk '/Up$/{print $2}' > aqua_hosts.txt
+nmap -sn -iL "$victim" -oG - | awk '/Up$/{print $2}' > aqua_hosts.txt
 nmap -T5 -iL aqua_hosts.txt --top-ports 10000 --exclude-ports 502 -oX scan.xml
-read -r -s -p $'Press enter to continue...'
+read -p "hit enter to continue (make sure jvis is up)..." confirm
 wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip -O aquatone.zip
 mkdir aqua
 unzip aquatone.zip
